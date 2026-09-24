686fc6d223a4d23f350797b2c8553018297e2c65
davedev294 <dalxirp294@gmail.com>
2026-09-24 16:09:03 +0200

    .

--- services/mailer.py
+++ services/mailer.py
@@ -8,24 +8,16 @@ class Mailer:
         with open (file, "r", encoding="utf-8") as file:
             return file.read()
 
-    def send_test(self, email):
-
-        print(f"Enviando correo de prueba a {email}...")
-        print("Correo de prueba enviado correctamente.")
-
-        if email == "pedro@ejemplo.com":
-            raise Exception("Simulación de error al enviar correo.")
-
-    
-        return True
-
     def send_email(self, subject, newsletter, sender, recipient, password):
         msg = MIMEText(newsletter, "html", "utf-8")
         msg['Subject'] = subject
         msg['From'] = sender
         msg['To'] = recipient
 
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smpt_server:
+        with smtplib.SMTP('smtp.gmail.com', 587) as smpt_server:
+            smpt_server.ehlo()
+            smpt_server.starttls()
+            smpt_server.ehlo()
             smpt_server.login(sender, password)
             smpt_server.sendmail(sender, recipient, msg.as_string())
 
